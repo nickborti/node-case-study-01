5b2a33df5ef616a6f1cc024cdcf9f50c13b70cfa
nickborti <saaggy18@gmail.com>
2020-09-09 16:59:51 +0530

    ïnitial commit: Model User/Role + Sequelize DB connection

--- router/auth.js
+++ router/auth.js
@@ -3,29 +3,27 @@ const express = require('express');
 const db = require('./../config/db.config');
 const User = require('./../model/User')(db);
 const Role = require('./../model/Role')(db);
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 const router = express.Router();
 
-router.
-    route('/test')
-    .get((req, res) => {
-        res.json({ success: true });
-    });
+router.route('/test').get((req, res) => {
+  res.json({ success: true });
+});
 
-router.
-    route('/signup')
-    .post((req, res) => {
-        const { name, username, email, password } = req.body;
-        User.create({
-            name, username, email, password
-        }).then(user => {
-                user.setRoles(req.body.roles).then(() => {
-                res.send({ message: 'Registered successfully!' });
-            });
-        }).catch(err => {
-            res.status(500).send({ reason: err.message });
-        });
-          
+router.route('/signup').post((req, res) => {
+  const { name, username, email, password } = req.body;
+  User.create({
+    name,
+    username,
+    email,
+    password,
+  })
+    .then((user) => {
+      console.log('registration process.');
     })
+    .catch((err) => {
+      res.status(500).send({ reason: err.message });
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+require('./config/db.config');
 
 // Routes
 const authRoute = require('./router/auth');
 const testRoute = require('./router/test');
 
 app.use('/api/auth', authRoute);
-
-require('./config/db.config');
 
 app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
